619117fa43f37171ef9e59de65e270c7b897cbf1
Zachary <420355244@qq.com>
2018-05-07 21:59:59 +0800

    修改restfulcrud的web相关逻辑以及页面改为thymeleaf

--- springboot-learning-web-restfulcrud/src/main/java/com/zzm/springboot/config/MyMvcConfig.java
+++ springboot-learning-web-restfulcrud/src/main/java/com/zzm/springboot/config/MyMvcConfig.java
@@ -1,11 +1,26 @@
 package com.zzm.springboot.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class MyMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        super.addViewControllers(registry);
+        registry.addViewController("/success").setViewName("success");
+    }
+
+    @Bean
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter(){
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+            }
+        };
+        return adapter;
     }
 }
